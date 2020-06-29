739dee17394da26770191a827972367cce043d3e
Daniil <danik22122005@gmail.com>
2020-06-29 17:37:27 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //1
+//        for (int i=args.length-1;i>=0;i--){
+//            System.out.println(args[i]);
+//        }
+
+        //2 (dukii kolhoz)
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Password: ");
+//        String passwd = in.nextLine();
+//
+//        String passwdCheck = "Qwerty-1";
+//        if (passwd.equals(passwdCheck)) {
+//            System.out.println("true");
+//        } else {
+//            System.out.println("false");
+//        }
+
+        //3
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("nums: ");
+//        String nums = in.nextLine();
+//        String[] n = nums.split(" ");
+//        int sum=0;
+//        for (String el:n){
+//            sum+=Integer.parseInt(el);
+//        }
+//        System.out.print("Sum: "+sum);
+
+    }
+}
+
